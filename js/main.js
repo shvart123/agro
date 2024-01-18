d3169d004602d9be10d35c8dafa741837705554d
shvart123 <132262731+shvart123@users.noreply.github.com>
2024-01-18 21:40:45 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "./img/icons/nav-close.svg";
+    } else {
+        navBtnImg.src = './img/icons/nav-open.svg';
+    }
+}
+function showImage(imageSrc) {
+  let popupImage = document.getElementById("popupImage");
+  popupImage.src = imageSrc;
+   
+  let imagePopup = document.getElementById("imagePopup");
+  imagePopup.style.display = "block";
+  document.body.style.overflow = "hidden";
+}
+// function to hide the image when we click on cross button
+function closeImage() {
+  let imagePopup = document.getElementById("imagePopup");
+  imagePopup.style.display = "none";
+ document.body.style.overflow = "auto";
+}
+AOS.init();
